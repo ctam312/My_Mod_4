c5063e9b39b7eee1be57aec80c71d9c78988bd77
Ctam312 <ctam312@gmail.com>
2022-11-30 11:05:50 -0500

    week10 day 1 stuff

---
+++ w10/d1/1-practice-for-week-10-intro-to-express-main/server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Create the Express app.
+const app = express();
+
+app.get('/status', (req, res) => {
+    res.send('Weiner from the app!')
+})
+
+const port = 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
--- w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
+++ w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
@@ -25,6 +25,23 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//   console.log('Body:', req.body);
+//   next();
+// });
+
+app.get('/artists', (req,res) => {
+  res.status(200);
+  res.json(getAllArtists())
+})
+
+app.post('/artists', (req, res) => {
+  // console.log(req.body)
+  const data = addArtist(req.body)
+  res.json(data)
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
