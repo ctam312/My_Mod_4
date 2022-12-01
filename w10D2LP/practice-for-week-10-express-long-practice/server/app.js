7df2602a02d0fa34c8f6b35b463e61d1d42aba82
Ctam312 <ctam312@gmail.com>
2022-12-01 11:18:03 -0500

    day 3 items

---
+++ w10D2LP/practice-for-week-10-express-long-practice/server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('express-async-errors');
+const app = express();
+
+app.use(express.static('assets'))
+app.use(express.json())
+
+
+app.use('/', (req,res) => {
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`)
+  res.on('finish', () => {
+    console.log(res.statusCode)
+  })
+})
+
+// For testing purposes, GET /
+app.get('/', (req, res) => {
+  res.json("Express server running. No content provided at root level. Please use another route.");
+});
+
+
+// For testing express.json middleware
+app.post('/test-json', (req, res, next) => {
+  // send the body as JSON with a Content-Type header of "application/json"
+  // finishes the response, res.end()
+  console.log(req.body)
+  res.json(req.body);
+  next()
+});
+
+// For testing express-async-errors
+app.get('/test-error', async (req, res) => {
+  throw new Error("Hello World!")
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
